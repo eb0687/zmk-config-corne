609d6cf183757a14accd7409a8ed87ea54f1aa17
eb0687 <ebrahim0687@gmail.com>
2025-10-26 11:47:32 +0300

    chore: add semantic-release configuration

---
+++ release.config.js
@@ -0,0 +1,24 @@
+const config = {
+  branches: ["master"],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/github",
+      {
+        assets: [{ path: "firmware/firmware.zip", label: "Firmware ZIP" }],
+      },
+    ],
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/**/*.{js,css}", "docs", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
+
+modeule.exports = config;
